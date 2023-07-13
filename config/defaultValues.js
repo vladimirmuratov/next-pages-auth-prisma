261f785a402b9dcfb265c4eab2edd3974aa06720
vladimir <vladimirmuratov@mail.ru>
2023-07-13 14:04:57 +0300

    fix url2

--- config/defaultValues.js
+++ config/defaultValues.js
@@ -1,3 +1,2 @@
 // export const BASE_URL = 'http://localhost:3000'
-// export const BASE_URL = 'https://test-auth-chakra.vercel.app/'
-export const BASE_URL = '/'
+export const BASE_URL = 'https://test-auth-chakra.vercel.app/'
