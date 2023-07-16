d3fb94dacf3613fdf48d5f08833b35f1f4c30b80
vladimir <vladimirmuratov@mail.ru>
2023-07-16 20:37:02 +0300

    orderBy asc

--- pages/api/message/index.js
+++ pages/api/message/index.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
                 ]
             },
             orderBy: {
-                createdAt: 'desc'
+                createdAt: 'asc'
             }
         })
 
